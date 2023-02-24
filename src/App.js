f8fbd4afb24302c837935dce34f59e9753819d17
Nick Abate <nick.abate@outlook.com>
2023-02-24 12:24:12 -0500

    Routes and page layout.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,56 @@
 import "./Home.scss";
 
+import github from "../../assets/images/socials/github.png";
+import linkedin from "../../assets/images/socials/linkedin.png";
+import twitter from "../../assets/images/socials/twitter.png";
+
 export default function Home() {
   return (
     <>
-      <header>Test header</header>
-      <main>Test main</main>
+      <header className="header">
+        <a href="/">Nick Abate</a>
+        <a href="/">Home</a>
+        <a href="/">Portfolio</a>
+        <a href="/">Skills</a>
+        <a href="/">Experience</a>
+        <button>Contact Me</button>
+      </header>
+
+      <main>
+        <section className="hero">
+          <h1>Hi! 👋 Welcome to my page.</h1>
+          <p>
+            I'm a Full Stack Software Engineer who transitioned from working as
+            a Chartered Professional Accountant in the world of accounting and
+            finance.
+          </p>
+        </section>
+        <section className="portfolio">
+          <h2>Portfolio</h2>
+          <p>Take a look at some of my work below.</p>
+        </section>
+        <section className="skills">
+          <h2>Skills</h2>
+          <p>
+            Some of the languages, frameworks and libraries I've worked with
+            include the following:
+          </p>
+        </section>
+        <section className="experience">
+          <h2>Experience</h2>
+          <p>I've worked as the following.</p>
+        </section>
+        <section className="blog">
+          <h2>Writing</h2>
+          <p>Here's some things which I've wrote about.</p>
+        </section>
+      </main>
+      <footer>
+        <h2>Get in touch with me!</h2>
+        <img src={github} alt="github" />
+        <img src={linkedin} alt="linkedin" />
+        <img src={twitter} alt="twitter" />
+      </footer>
     </>
   );
 }
