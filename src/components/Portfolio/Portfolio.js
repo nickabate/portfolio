92bb7e862feba1a164458f278a0b95d0597bce7e
Nick Abate <nick.abate@outlook.com>
2023-03-21 23:50:51 -0400

    Update portfolio project showcase.

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,4 +1,6 @@
 import "./Portfolio.scss";
+import glucoast from "../../assets/images/projects/glucoast.png";
+import github from "../../assets/images/tech/github.png";
 
 export default function Portfolio() {
   return (
@@ -14,7 +16,55 @@ export default function Portfolio() {
             to do so!
           </p>
           <div className="portfolio__projects">
-            Projects will be updated soon...
+            <div className="portfolio__showcase">
+              <div className="portfolio__sample">
+                <img
+                  className="portfolio__image"
+                  src={glucoast}
+                  alt="GLUCOAST sample dashboard"
+                />
+              </div>
+              <div className="portfolio__details">
+                <h3>GLUCOAST</h3>
+                <p className="portfolio__body">
+                  GLUCOAST is an all-in-one dashboard to assist diabetics in
+                  managing their glucose levels throughout the day.
+                </p>
+                <p className="portfolio__body">
+                  Tech stack includes: HTML, Css, Js, Sass, React, Node, and
+                  Express.
+                </p>
+                <div className="portfolio__repo">
+                  <a
+                    href="https://github.com/nickabate/glucoast-client"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      className="portfolio__github"
+                      src={github}
+                      alt="github repo link"
+                    />
+                  </a>
+                  <a
+                    className="portfolio__link"
+                    href="https://github.com/nickabate/glucoast-client"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Client source code
+                  </a>
+                  <a
+                    className="portfolio__link portfolio__link--break"
+                    href="https://github.com/nickabate/glucoast-server"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Server source code
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
