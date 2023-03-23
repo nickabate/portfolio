732b0cf47b5583388f8ee2590919a421b7540a33
Nick Abate <nick.abate@outlook.com>
2023-03-23 12:24:10 -0400

    Add additional font colour option and apply throughout.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,14 +8,11 @@ export default function Hero() {
         <img className="hero__image" src={nick} alt="Nick Abate" />
       </div>
       <div className="hero__container">
-        <h1 className="hero__greeting">
-          Hi! Welcome to my little corner of the internet.
-        </h1>
+        <h1 className="hero__greeting">Hi! Welcome to my page.</h1>
         <p className="hero__text">
-          I'm Nick Abate, a Full Stack Software Engineer and BrainStation
-          Software Engineering Bootcamp Alumni. I chose to pursue Software
-          Engineering to come up with innovative solutions to everyday problems
-          experienced by everyday people.
+          I'm Nick Abate, a software engineer and bootcamp graduate. I'm
+          pursuing software engineering to come up with innovative solutions to
+          everyday problems experienced by everyday people.
         </p>
       </div>
     </section>
